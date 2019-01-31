9f0cf97e2d7628ea974f0386f0735e57877d2f86
Ryan <RyanJamesGroom@gmail.com>
2019-01-31 12:50:01 -0500

    Updated NavBar

---
+++ index.js
@@ -0,0 +1,10 @@
+function toggleNav() {
+    let nav = document.getElementById('navBar');
+
+    if (nav.style.width == "30%") {
+        nav.style.width = "80px";
+    }
+    else {
+        nav.style.width = "30%";
+    }
+}
