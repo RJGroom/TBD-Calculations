4318c020de636fd8d4e37263b87cdfa1c954193a
Ryan <RyanJamesGroom@gmail.com>
2019-02-06 12:21:44 -0500

    Added navbar to budget assessment page

--- budgetAssessment/index.js
+++ budgetAssessment/index.js
@@ -1,4 +1,5 @@
 var income = 0;
+var monthlyIncome = income/12;
 var primaryExpenses = 0;
 var primaryExpensesRate = 0;
 var utilities = 0;
@@ -7,6 +8,44 @@ var secondaryExpenses = 0;
 var secondaryExpensesRate = 0;
 var savings = 0;
 var savingsRate = 0;
+var excessFunds = income - primaryExpenses - utilities - secondaryExpenses - savings
+
+//Open Menu Function
+function toggleNav() {
+    let nav = document.getElementById('navBar');
+    let topBar = document.getElementById('topBar');
+    let middleBar = document.getElementById('middleBar');
+    let bottomBar = document.getElementById('bottomBar');
+
+    if (nav.style.width == "23%") {
+        nav.style.width = "70px";
+        topBar.style.width = "";
+
+        bottomBar.style.transform = "rotate(0deg)";
+        topBar.style.transform = "rotate(0deg)";
+        middleBar.style.opacity = "1";
+
+        setTimeout (function(){
+            bottomBar.style.top = "20px";
+            topBar.style.top = "0px";
+        }, 500)
+
+    }
+    else {
+        nav.style.width = "23%";
+        topBar.style.width= "100%";
+
+        topBar.style.top = "10px";
+        bottomBar.style.top = "10px";
+
+        setTimeout (function(){
+            middleBar.style.opacity = "0";
+            topBar.style.transform = "rotate(45deg)";
+            bottomBar.style.transform = "rotate(-45deg)";
+        }, 500)
+    }
+}
+
 
 function printReport() {
 
@@ -14,6 +53,7 @@ function printReport() {
     income =
     parseInt(document.getElementById("primary-income").value) + parseInt(document.getElementById("secondary-income").value);
     document.getElementById("income-report").innerHTML = income;
+    document.getElementById("income-report-monthly").innerHTML = income/12;
 
     //Primary expenses
     primaryExpenses =
@@ -62,3 +102,4 @@ function printReport() {
     savingsRate = (savings / income) * 100;
     document.getElementById("savings-rate").innerHTML = savingsRate;
 }
+
