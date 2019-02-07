6e7ff246d5215b15f1c124593ea60cf99136fbe7
rhrv <rhrv@iup.edu>
2019-02-07 13:23:30 -0500

    dfd

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+include 'comm.php';
+
+$Username = $_REQUEST['loginUsername'];
+$Password = $_REQUEST['loginPassword'];
+
+$sql = "SELECT EXISTS(SELECT * FROM users WHERE username = '$Username' AND password = '$Password')";
+$result = $conn->query($sql);
+
+if (empty($arr))
+{
+    echo "login failed.";
+}
+else
+{
+    echo "login successful. Welcome " . $Username;
+}
+?>
