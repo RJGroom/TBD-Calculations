451223109b2e54c04000f23b0777e637d2cbb2c3
Ryan Groom <ryan@wifi-135-74.nwpb.iup.edu>
2019-03-05 13:31:11 -0500

    Fixed Login Background

--- login/login.php
+++ login/login.php
@@ -113,7 +113,11 @@
         <input class= "loginInput" type= "password" name= "password" placeholder= "Password" size= "27"><br/>
         <input class= "loginInput loginBtn" name= "login" type= "submit" value= "Login"/><br/>
     </form>  
+
+    <div class="register-div">
     <a href= "./register.php"><button class="registerBtnLogin">Register</button></a>
+    </div>
+
     <script src="login.js"></script>
 </body>
 </html>
