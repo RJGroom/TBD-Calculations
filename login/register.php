6d4113a69fb73a6d8322b6b124784e4145cf1a2e
rhrv <rhrv@iup.edu>
2019-04-02 18:08:31 -0400

    Attempted email and fixed registration

--- login/register.php
+++ login/register.php
@@ -9,12 +9,20 @@ $InitPassword = $_REQUEST['initPassword'];
 $ConPassword = $_REQUEST['confirmPassword'];
 $sql = "SELECT * FROM users WHERE username = '$Username'";
 $result = $conn->query($sql);
-if($InitPassword <> $ConPassword)
+
+if(empty($Username) || empty($FirstName) || empty($LastName) || empty($Email) || empty($InitPassword) || empty($ConPassword))
 {
+    header('Refresh: 5; registration.php');
+    echo "Please enter in all information";
+}
+else if($InitPassword <> $ConPassword)
+{
+    header('Refresh: 5; registration.php');
     echo "Passwords do not match.";
 }
 else if($result->num_rows > 0)
 {
+    header('Refresh: 5; registration.php');
     echo "Username already taken. Please try another.";
 }
 else
@@ -24,11 +32,24 @@ else
     $result = $conn->query($sql2);
     if($conn->query($sql) === TRUE)
     {
-        header("Location: ../index.php");   
+        session_start();
+        include_once("comm.php");
+
+        $sql = "SELECT * FROM users WHERE username = '$Username' LIMIT 1";
+        $query = mysqli_query($conn, $sql);
+        $row =  mysqli_fetch_array($query);
+        $id = $row['id'];
+        $db_password = $row['password'];
+
+            $_SESSION['username'] = $Username;
+            $_SESSION['id'] = $id;
+            $_SESSION['isLoggedIn'] = TRUE;
+            header("Location: ../profile/profile.php");
+       // header("Location: ../index.php");
     }
     else
     {
-	    echo "Sorry could not create user";
+        echo "Sorry could not create user";
     }
 }
 ?>
