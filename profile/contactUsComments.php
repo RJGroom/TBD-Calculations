56bcf5e834528ff96ca0fe3a585957d84efaf228
ERitchey <eritchey93@outlook.com>
2019-04-25 13:33:56 -0400

    Added style to admin comments page

--- profile/contactUsComments.php
+++ profile/contactUsComments.php
@@ -13,21 +13,30 @@
 </head>
 <body>
     <div class="comments-div">
-        <h3 class="comments-header"></h3>
+        <h3 class="comments-header">Comments</h3>
         <p class="comments-content">
             <?php
             include "comm.php";
             $sql = "SELECT * FROM contact";
             $result = $conn->query($sql);
 
+            ?>
+            <ul>
+            <?php
             if($result->num_rows > 0)   {
                 while($row = $result->fetch_assoc())    {
-                    echo $row["ContactName"] . " whose email is " . $row["ContactEmail"] . " said this: <br>";
-                    echo $row["ContactComment"] . "<br>";
+                    ?>
+                        <li><?php echo $row["ContactName"] . " whose email is " . $row["ContactEmail"] . " said this: <br>"; ?>
+                        <span class="user-comment"><?php echo "&quot;" . $row["ContactComment"] . "&quot;" . "<br>"; ?></span></li>
+                    <?php
                 }
             }
             ?>
+            </ul>
 
         </p>
+        <a href="../profile/profile.php">
+            <button class="back-btn">Back to Profile</button>
+        </a>
     </div>
 </body>
