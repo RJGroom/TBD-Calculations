6d6b97bde6878ccea4f2c58b42f72d1655a88151
Ryan <RyanJamesGroom@gmail.com>
2019-04-30 12:24:46 -0400

    Fixed some broken links

--- login/registration.php
+++ login/registration.php
@@ -6,8 +6,88 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Open+Sans|Oswald|Raleway|Roboto" rel="stylesheet">
     <title>Register</title>
+    <link rel="stylesheet" href="../node_modules/normalize.css/normalize.css">
+    <link rel="stylesheet" href="../navbar/navbarstyle.css">
     <link rel="stylesheet" href="style.css">
 </head>
+
+<div class="navBar" id="navBar">
+
+        <div class="navSection">
+            <h3 class="menu-text" id="menuText">Menu</h3>
+            <div class="menuIcon" onclick="toggleNav()" title = "Open Menu">
+                <div class="menuBar topBar" id="topBar"></div>
+                <div class="menuBar middleBar" id="middleBar"></div>
+                <div class="menuBar bottomBar" id="bottomBar"></div>
+            </div>
+        </div>
+
+        <div class="navSection">
+            <p class="navDescription" id="navTextOne">Go back to our <span style="font-weight:bold">homepage</span></p>
+            <a href="../index.php" class="navLink">
+                <img class="navIcon" src="../Icons/house-outline.svg" title="Home">
+            </a>
+        </div>
+
+        <div class="navSection">
+            <p class="navDescription" id="navTextTwo"><span style="font-weight:bold">Log in</span> to save your records and keep track of your spending habits</p>
+            <a href=
+            "
+            <?php
+                if(isset($_SESSION['username'])){       
+                    echo "../profile/profile.php";
+                }
+                else echo "../login/login.php";
+                ?>
+            " class="navLink">
+                <img class="navIcon" src="../Icons/006-login-square-arrow-button-outline.svg" title="Login">
+            </a>
+        </div>
+
+        <div class="navSection">
+        <p class="navDescription">Fill out your information for a <span style="font-weight:bold">one-time</span> assessment
+                of your budget</p>
+            <a href="../budgetAssessment/budgetAssessment.html" class="navLink">
+                <img class="navIcon" src="../Icons/005-graph-1.svg" title="View-Graph">
+            </a>
+        </div>
+
+        <div class="navSection">
+            <p class="navDescription" id="navTextFour">View a list of <span style="font-weight:bold">budgeting tips</span> and advice to help improve your spending habits</p>
+            <a href="
+            <?php
+            if(isset($_SESSION['username'])){  
+                if ($userInfo['isAdmin'] == 1) {
+                    echo "../tips/tipsadmin.php";
+                }
+                else {
+                    echo "../tips/tips.php";
+                }
+            }
+            else {
+                echo "../tips/tips.php";
+            }
+            ?>
+            " 
+            class="navLink">
+                <img class="navIcon" src="../Icons/007-elemental-tip.svg" title="Budgeting-Tips">
+            </a>
+        </div>
+
+        <div class="navSection">
+            <p class="navDescription" id="navTextFive"><span style="font-weight:bold">Contact</span> TBD Calculations with any of your questions, comments, or concerns</p>
+            <a href ="./contact/contact.php" class="navLink">
+                <img class="navIcon" src="../Icons/001-contact.svg" title="Contact">
+            </a>
+        </div>
+
+        <div class="navSection">
+            <p class="navDescription" id="navTextSix">View a list of the different <span style="font-weight:bold">language and currency</span> preferences available</p>
+            <img class="navIcon" src="../Icons/008-worlwide.svg" title="Language-Currency">
+        </div>
+
+    </div>
+
 <body class="registrationPage">
     <div class="registration-div">
     <p class="registrationHeader">Register</p>
